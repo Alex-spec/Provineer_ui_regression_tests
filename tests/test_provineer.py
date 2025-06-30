820477fc5f03ddf3f30a41bf4ad78d4d95bf4cdb
Alex Spec <alexyankovskio@gmail.com>
2025-06-30 14:01:28 +0200

    allure changings

--- tests/test_provineer.py
+++ tests/test_provineer.py
@@ -1,26 +1,33 @@
-import time
+import allure
 from Provineer_regression_tests.pages.login_page import LoginPageSteps
 from Provineer_regression_tests.pages.dashboard_page import DashboardPageSteps
 from Provineer_regression_tests.pages.upload_page import UploadPageSteps
 
-
+@allure.description("Test upload new file")
 def test_upload_new_file(driver):
     print("Start checking new file upload")
     lp = LoginPageSteps(driver)
     lp.login_page()
     up = UploadPageSteps(driver)
     up.upload_page()
+    print("End checking new file upload")
 
+@allure.description("Test shared feature")
 def test_shared_feature(driver):
+    print("Start checking shared feature")
     lp = LoginPageSteps(driver)
     lp.login_page()
     dp = DashboardPageSteps(driver)
     dp.test_uploads_dashboard()
+    print("End checking shared feature")
 
+@allure.description("Test proofs and certificates")
 def test_proofs_and_certificates(driver):
+    print("Start checking proofs and certificates")
     lp = LoginPageSteps(driver)
     lp.login_page()
     dp = DashboardPageSteps(driver)
     dp.check_e_certificate_feature()
+    print("End checking proofs and certificates")
 
 
